c5e5fccf79f70dad4ab0e68fc4d226e975c8ec32
Tay Zheng Qiang <tay@outlook.my>
2020-02-20 09:11:45 +0800

    Add Recipe array [ Page.ts ]

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://via.placeholder.com/150',
+      ingredients: ['French Fries', 'Portk Meat', 'Salad'] ,
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://via.placeholder.com/150',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'] ,
+    },
+    {
+      id: 'r2',
+      title: 'gfdgda',
+      imageUrl: 'https://via.placeholder.com/150',
+      ingredients: ['sadasdaa', 'asadasd', 'sadsada'] ,
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
